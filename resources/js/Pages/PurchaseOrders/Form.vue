dca6a375a67f84426765f6f9d3c7a54e3655f683
Ariefan Dipokusumo Wibowo <ariefandw@gmail.com>
2024-10-30 01:36:17 +0700

    Refactor PurchaseOrders form to simplify input fields and add price and subtotal calculations

--- resources/js/Pages/PurchaseOrders/Form.vue
+++ resources/js/Pages/PurchaseOrders/Form.vue
@@ -142,62 +142,6 @@ watch(() => newProduct.value.product_id, async (newVal) => {
                 <h2 class="font-semibold text-lg text-gray-800 dark:text-gray-200 leading-tight my-4">
                     Daftar Barang
                 </h2>
-                <div class="grid grid-cols-4 gap-4">
-                    <div>
-                        <InputLabel for="product_id" value="Barang" />
-                        <AutoComplete v-model="newProduct.product_id" :label="newProduct.product_label"
-                            @update:label="newProduct.product_label = $event" apiUrl="/api/products/suggestions" />
-                        <!-- <AutoComplete v-model="newProduct.product_id" :label="newProduct.product_label"
-                            @update:label="newProduct.product_label = $event" api-url="/api/products/suggestions">
-                            <template #suggestion-item="{ suggestion, highlighted }">
-                                <li :class="['cursor-pointer px-4 py-2 flex items-center', { 'bg-blue-200': highlighted }]"
-                                    @click="$emit('click', suggestion)" @mousedown="$emit('mousedown', suggestion)">
-                                    <p class="text-md font-semibold">{{ suggestion.product_name }}</p>
-                                    <p class="text-sm text-gray-500 dark:text-gray-400">
-                                        {{ suggestion.brand.brand_name }}
-                                    </p>
-                                </li>
-                                <hr class="my-1">
-                            </template>
-                        </AutoComplete> -->
-                    </div>
-                    <div>
-                        <InputLabel for="brand" value="Merk" />
-                        <p class="dark:text-white mt-2">{{ productDetails?.brand?.brand_name }}</p>
-                    </div>
-                    <div>
-                        <InputLabel for="unit" value="Kategori" />
-                        <p class="dark:text-white mt-2">{{ productDetails?.tag }}</p>
-                    </div>
-                    <!-- <div>
-                        <InputLabel for="quantity" value="Jumlah" />
-                        <TextInput type="number" id="quantity" v-model="newProduct.quantity" />
-                        <InputError :message="form.errors.quantity" />
-                    </div>
-                    <div>
-                        <InputLabel for="unit" value="Satuan" />
-                        <TextInput id="unit" v-model="newProduct.unit" />
-                        <InputError :message="form.errors.unit" />
-                    </div> -->
-                    <div>
-                        <InputLabel for="unit" value="&nbsp;" />
-                        <Button type="button" color="green" @click="addRow">
-                            Tambah
-                        </Button>
-                    </div>
-                </div>
-                <div class="mt-2">
-                    <InputLabel for="unit" value="Spesifikasi" />
-                    <p class="dark:text-white mt-2">{{ productDetails?.product_description }}&nbsp;</p>
-                </div>
-
-                <!-- Display product details returned from the API -->
-                <!-- <div v-if="productDetails" class="mt-2 text-dark dark:text-white">
-                    <p>ID: {{ productDetails.id }}</p>
-                    <p>Kategori: {{ productDetails.tag }}</p>
-                    <p>Merk: {{ productDetails.brand.brand_name }}</p>
-                    <p>Spesifikasi: {{ productDetails.product_description }}</p>
-                </div> -->
 
                 <div class="relative overflow-x-auto overflow-y-hidden shadow-md sm:rounded-lg mt-2">
                     <table class="w-full text-sm text-left rtl:text-right text-gray-700 dark:text-gray-300">
@@ -206,6 +150,8 @@ watch(() => newProduct.value.product_id, async (newVal) => {
                                 <th class="px-4 py-3">Barang</th>
                                 <th class="px-4 py-3">Jumlah</th>
                                 <th class="px-4 py-3">Satuan</th>
+                                <th class="px-4 py-3">Harga</th>
+                                <th class="px-4 py-3">Subtotal</th>
                                 <th></th>
                             </tr>
                         </thead>
@@ -214,16 +160,20 @@ watch(() => newProduct.value.product_id, async (newVal) => {
                                 class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                                 <td class="px-4 py-1">{{ item.product_label }}</td>
                                 <td class="px-4 py-1">
-                                    <TextInput class="py-1" type="number" v-model="item.quantity" />
+                                    <TextInput class="py-1" type="hidden" v-model="item.quantity" />
+                                    <p class="py-1">{{ item.quantity }}</p>
                                 </td>
                                 <td class="px-4 py-1">
-                                    <!-- <TextInput class="py-1" type="text" v-model="item.unit" /> -->
-                                    <Select v-model="item.unit">
-                                        <option v-for="option in satuanOptions" :value="option.value"
-                                            :key="option.value">
-                                            {{ option.label }}
-                                        </option>
-                                    </Select>
+                                    <TextInput class="py-1" type="hidden" v-model="item.unit" />
+                                    <p class="py-1">
+                                        {{ satuanOptions.find(option => option.value === item.unit)?.label }}
+                                    </p>
+                                </td>
+                                <td class="px-4 py-1">
+                                    <TextInput class="py-1" type="number" v-model="item.price" />
+                                </td>
+                                <td class="px-4 py-1">
+                                    <p class="py-1">{{ (item.quantity * item.price).toFixed(2) }}</p>
                                 </td>
                                 <td class="px-4 py-1">
                                     <Button type="button" color="red" @click="removeRow(index)">
