d8b2a5daa9f0ef74b3d0eb160a6dab12c39369d2
Ariefan Dipokusumo Wibowo <ariefandw@gmail.com>
2024-12-12 06:24:19 +0700

    Remove foreign key checks from seeders for cleaner database seeding

--- database/seeders/BrandSeeder.php
+++ database/seeders/BrandSeeder.php
@@ -7,14 +7,14 @@ use Illuminate\Database\Seeder;
 
 class BrandSeeder extends Seeder
 {
-	/**
-	 * Run the database seeds.
-	 */
-	public function run(): void
-	{
-		\DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-		Brand::truncate();
-		\DB::statement('SET FOREIGN_KEY_CHECKS=1;');
-		Brand::factory()->count(15)->create();
-	}
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // \DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        Brand::truncate();
+        // \DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        Brand::factory()->count(15)->create();
+    }
 }
